91bc54b35aae3269ede90dea913e11bb8ac4612a
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-08-06 23:01:05 -0400

    Create route for viewing individual messages

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const indexController = require("../controllers/indexController");
 
 /* GET home page. */
-router.get("/", indexController.listAllMessagesGet);
+router.get("/", indexController.listAllMessages);
 
 router.get("/new", indexController.createMessageGet);
 
 router.post("/new", indexController.createMessagePost);
+
+router.get("/message/:id", indexController.getMessage);
 
 module.exports = router;
