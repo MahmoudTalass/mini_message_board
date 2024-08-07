f4d39c1e751e938c38cc395c02228cbadc8e79b2
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-08-06 22:59:33 -0400

    Use and work with db data instead of static data

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,27 +1,29 @@
-async function listAllMessagesGet(req, res) {
+const db = require("../db/query");
+
+async function listAllMessages(req, res) {
+   const messages = await db.getAllMessages();
    res.render("index", { title: "Mini Messageboard", messages });
 }
 
 async function createMessageGet(req, res) {
-   res.render("form");
+   res.render("form", { title: "New Message Form" });
 }
 
 async function createMessagePost(req, res) {
-   const formElements = req.body;
-
-   const newMessage = {
-      text: formElements.text,
-      user: formElements.user,
-      added: new Date(),
-   };
-
-   messages.push(newMessage);
-
+   const { text, username } = req.body;
+   await db.createMessage({ username, text });
    res.redirect("/");
 }
 
+async function getMessage(req, res) {
+   const { id } = req.params;
+   const message = await db.getMessage(id);
+   res.render("message", { title: `Message by: ${message[0].username}`, message: message[0] });
+}
+
 module.exports = {
-   listAllMessagesGet,
+   listAllMessages,
    createMessageGet,
    createMessagePost,
+   getMessage,
 };
